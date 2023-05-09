de9e3a95c4fb7d56ad44c0e5812c31f53b8dc740
pojiro <pojiro.jp@gmail.com>
2023-05-09 14:35:02 +0900

    p.173 まで実装

--- src/main/java/SnipersTableModel.java
+++ src/main/java/SnipersTableModel.java
@@ -4,7 +4,7 @@ import auctionsniper.SniperSnapshot.SniperState;
 import javax.swing.table.AbstractTableModel;
 
 public class SnipersTableModel extends AbstractTableModel {
-    private static final SniperSnapshot STARTING_UP = new SniperSnapshot("", 0, 0, SniperState.JOINING);
+    private static final SniperSnapshot STARTING_UP = SniperSnapshot.joining("");
     private SniperSnapshot sniperSnapshot = STARTING_UP;
 
     @Override
@@ -19,18 +19,7 @@ public class SnipersTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        switch (Column.at(columnIndex)) {
-            case ITEM_IDENTIFIER:
-                return sniperSnapshot.itemId;
-            case LAST_PRICE:
-                return sniperSnapshot.lastPrice;
-            case LAST_BID:
-                return sniperSnapshot.lastBid;
-            case SNIPER_STATE:
-                return textFor(sniperSnapshot.state);
-            default:
-                throw new IllegalArgumentException("No column at " + columnIndex);
-        }
+        return Column.at(columnIndex).valueIn(sniperSnapshot);
     }
 
     private final static String[] STATUS_TEXT = {"Joining", "Bidding", "Winning", "Lost", "Won"};
@@ -45,7 +34,29 @@ public class SnipersTableModel extends AbstractTableModel {
     }
 
     public enum Column {
-        ITEM_IDENTIFIER, LAST_PRICE, LAST_BID, SNIPER_STATE;
+        ITEM_IDENTIFIER {
+            @Override
+            public Object valueIn(SniperSnapshot sniperSnapshot) {
+                return sniperSnapshot.itemId;
+            }
+        }, LAST_PRICE {
+            @Override
+            public Object valueIn(SniperSnapshot sniperSnapshot) {
+                return sniperSnapshot.lastPrice;
+            }
+        }, LAST_BID {
+            @Override
+            public Object valueIn(SniperSnapshot sniperSnapshot) {
+                return sniperSnapshot.lastBid;
+            }
+        }, SNIPER_STATE {
+            @Override
+            public Object valueIn(SniperSnapshot sniperSnapshot) {
+                return textFor(sniperSnapshot.state);
+            }
+        };
+
+        abstract public Object valueIn(SniperSnapshot sniperSnapshot);
 
         public static Column at(int offset) {
             return values()[offset];
